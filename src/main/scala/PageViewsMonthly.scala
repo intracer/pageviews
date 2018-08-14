660633db27f5469bd8375bc17c4d3a4f19c6ca3e
Ilya <intracer@gmail.com>
2018-08-14 23:07:45 +0300

    per https://uk.wikibooks.org/wiki/Ammonite//Статистика відвідувань вікісторінок

--- src/main/scala/PageViewsMonthly.scala
+++ src/main/scala/PageViewsMonthly.scala
@@ -1,46 +1,39 @@
+import io.circe._, io.circe.parser._, io.circe.generic.auto._
 import cats.implicits._
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.parser._
-
 import scala.io.Source
 
-object PageViewsMonthly {
+case class PageViews(article: String, views: Int)
 
-  def main(args: Array[String]): Unit = {
-    println(monthlyViews)
+def getMonthlyViews(project: String, year: Int, month: Int): Map[String, Int] = {
+  def url: String = {
+    val monthStr = (if (month < 10) "0" else "") + month
+    s"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/$project/all-access/$year/$monthStr/all-days"
   }
 
-  case class PV(article: String, views: Int)
+  val text = Source.fromURL(url).mkString
+  val doc = parse(text).getOrElse(Json.Null)
 
-  def monthlyViews = {
-
-    def getMonthlyViews(project: String, year: Int, month: Int): Map[String, Int] = {
-      def url: String = {
-        val monthStr = (if (month < 10) "0" else "") + month
-        s"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/$project/all-access/$year/$monthStr/all-days"
-      }
-
-      val text = Source.fromURL(url).mkString
-      val doc = parse(text).getOrElse(Json.Null)
-
-      (doc \\ "articles")
-        .flatMap(_.as[List[PV]].getOrElse(Nil))
-        .map(PV.unapply(_).get)
-        .toMap
-    }
-
-    def sumMapValues(maps: Seq[Map[String, Int]]) = maps.foldLeft(Map.empty[String, Int])(_ |+| _)
-
-    val monthly = (1 to 12).map { month => getMonthlyViews("uk.wikibooks.org", 2016, month) }
-
-    val yearly = sumMapValues(monthly)
-
-    val byBook = sumMapValues(
-      yearly.toSeq.map { case (name, views) => Map(name.split("/").head -> views) }
-    )
-
-    byBook.toSeq.sortBy(-_._2)
-  }
+  (doc \\ "articles")
+    .flatMap(_.as[List[PageViews]].getOrElse(Nil))
+    .map(PageViews.unapply(_).get)
+    .toMap
 }
 
+def sumMapValues(maps: Seq[Map[String, Int]]) = maps.foldLeft(Map.empty[String, Int])(_ |+| _)
+
+val monthly = (1 to 12).map { month => getMonthlyViews("uk.wikibooks.org", 2016, month) }
+
+val yearly = sumMapValues(monthly)
+
+val byBook = sumMapValues(
+  yearly.toSeq.map { case (name, views) => Map(name.split("/").head -> views) }
+)
+
+val wikiTable = byBook.toSeq
+  .filterNot(_._1.startsWith("Спеціальна:"))
+  .sortBy(-_._2)
+  .zipWithIndex.map {
+    case ((name, views), i) => s"| ${i + 1} || [[$name]] || $views"
+  }.mkString("{| class = \"wikitable\"\n|-\n", "\n|-\n", "\n|}")
+
+println(wikiTable)
